5c190d238be4e854ed27f6139b40b2f9c753aa77
Corey <corey.malcom@nwea.org>
2021-02-17 12:35:11 -0800

    another fix

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const GREETING = method => `Hello, ${method} Method!`;
 module.exports.handler = async event => {
   console.log(`Event log:  `, event);
-  const theMethod = event?.requestContext?.http?.method || "TEST";
+  const theMethod = event && event.requestContext && event.requestContext.http 
+    ? event.requestContext.http.method 
+    : 'TEST';
   return GREETING(theMethod);
 };
