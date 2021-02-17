428d1f84693c811208a1cd33a5cd753712f974f7
Corey <corey.malcom@nwea.org>
2021-02-17 14:37:59 -0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const GREETING = method => `Hello, ${method} Method!`;
-module.exports.handler = async event => {
+module.exports.greeting = async event => {
   console.log(`Event log:  `, event);
   const theMethod = event && event.requestContext && event.requestContext.http 
     ? event.requestContext.http.method 
